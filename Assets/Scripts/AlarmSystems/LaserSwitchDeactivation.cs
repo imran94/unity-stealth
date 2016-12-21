0b8065c6b85326c8f0ba0bda48eeae50f8113e19
blackmetall95 <rzcc5911@yahoo.co.uk>
2016-12-22 01:46:24 +0800

    Added Lasers

---
+++ Assets/Scripts/AlarmSystems/LaserSwitchDeactivation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSwitchDeactivation : MonoBehaviour {
+    public GameObject laser;
+    public Material unlockedMat;
+    private GameObject player;
+    private Renderer screenRend;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (Input.GetButton ("Switch"))
+            {
+
+            }
+        }
+    }
+
+    void LaserDeactivation()
+    {
+        laser.SetActive(false);
+
+        screenRend = transform.Find("prop_switchUnit_screen").GetComponent<Renderer>();
+        screenRend.material = unlockedMat;
+        GetComponent<AudioSource>().Play();
+
+    }
+}
