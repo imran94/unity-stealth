f9f597cf32a05c376ddcb7cbc56e85e04360fb22
imran94 <iraja094@gmail.com>
2016-12-21 23:46:50 +0800

    Create Animation Controller for Player

---
+++ Assets/Scripts/HashIDs.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+
+    public int dyingState, locomotionState, shoutState;
+    public int deadBool, sneakingBool, shoutingBool, playerInSightBool, openBool;
+    public int speedFloat, shotFloat, aimWeightFloat, angularSpeedFloat;
+
+    void Awake()
+    {
+        dyingState = Animator.StringToHash("Base Layer.Dying");
+        locomotionState = Animator.StringToHash("Base Layer.Locomotion");
+        shoutState = Animator.StringToHash("Shouting.Shout");
+
+        deadBool = Animator.StringToHash("Dead");
+        sneakingBool = Animator.StringToHash("Sneaking");
+        shoutingBool = Animator.StringToHash("Shouting");
+        playerInSightBool = Animator.StringToHash("PlayerInSight");
+        openBool = Animator.StringToHash("Open");
+
+        speedFloat = Animator.StringToHash("Speed");
+        shotFloat = Animator.StringToHash("Shot");
+        aimWeightFloat = Animator.StringToHash("AimWeight");
+        angularSpeedFloat = Animator.StringToHash("AngularSpeed");
+    }
+}
