e4e19e0c01fcdf99739b44c767eaeb5b2e69201a
imran94 <iraja094@gmail.com>
2016-12-22 01:08:50 +0800

    Implement player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public AudioClip shoutingClip;
+    public float turnSmoothing = 15f;
+    public float speedDampTime = 0.1f;
+
+    private Animator anim;
+    private HashIDs hash;
+    private Rigidbody rb;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        anim.SetLayerWeight(1, 1f);
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool sneak = Input.GetButton("Sneak");
+
+        ManageMovement(h, v, sneak);
+    }
+
+    void Update()
+    {
+        bool shout = Input.GetButtonDown("Attract");
+        anim.SetBool(hash.shoutingBool, shout);
+        ManageAudio(shout);
+    }
+
+    void ManageMovement(float horizontal, float vertical, bool sneak)
+    {
+        anim.SetBool(hash.sneakingBool, sneak);
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            Rotate(horizontal, vertical);
+            anim.SetFloat(hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
+        }
+        else
+        {
+            anim.SetFloat(hash.speedFloat, 0f);
+        }
+    }
+
+    void Rotate(float horizontal, float vertical)
+    {
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        Quaternion newRotation = Quaternion.Lerp(rb.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+        rb.MoveRotation(newRotation);
+    }
+
+    void ManageAudio(bool shout)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).fullPathHash == hash.locomotionState)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (shout)
+        {
+            AudioSource.PlayClipAtPoint(shoutingClip, transform.position);
+        }
+    }
+}
