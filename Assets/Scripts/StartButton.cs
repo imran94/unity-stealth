df8d9f6b33623d0d1c7aab47d50a9920272c22b3
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-01-13 00:09:39 +0800

    Menu added

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour {
+    public Button button_start;
+    Animator anim;
+
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+        anim = GetComponent<Animator>();
+        Button btn = button_start.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        anim.SetTrigger("MenuFade");
+        Time.timeScale = 1.0f;
+    }
+}
