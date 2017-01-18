e377e2d3092434ff3b5d6ca535933e03d9932304
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-01-19 04:34:23 +0800

    Level 2 Button

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour {
-    public Button button_start;
+    public Button button_lvl1;
+    public Button button_lvl2;
     Animator anim;
 
     void Start()
     {
         Time.timeScale = 0.0f;
         anim = GetComponent<Animator>();
-        Button btn = button_start.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        Button btn1 = button_lvl1.GetComponent<Button>();
+        Button btn2 = button_lvl2.GetComponent<Button>();
+        btn1.onClick.AddListener(StartLvl1);
+        btn2.onClick.AddListener(StartLvl2);
     }
 
-    void TaskOnClick()
+    void StartLvl1()
     {
         anim.SetTrigger("MenuFade");
         Time.timeScale = 1.0f;
+    }
+
+    void StartLvl2()
+    {
+        SceneManager.LoadScene("Scene 2");
     }
 }
