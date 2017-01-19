6c0a4a290cd697e785fb6a70bacb724ac96ccda5
imran94 <iraja094@gmail.com>
2017-01-19 14:34:57 +0800

    Create radar window

---
+++ Assets/Scripts/RadarCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarCamera : MonoBehaviour {
+
+    private Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag(Tags.player).GetComponent<Transform>();
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+        Debug.Log("Update RadarCamera: " + transform.position);
+    }
+}
