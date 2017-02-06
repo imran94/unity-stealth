7185338eb41b70af782f81c2284b9c707e91ecf6
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-02-06 21:43:03 +0800

    Simple Pickup
    Script and object in Scene 2

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject pickup;
+    private GameObject player;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (Input.GetButton("Switch"))
+            {
+                RemoveObject();
+            }
+        }
+    }
+
+    void RemoveObject()
+    {
+        pickup.SetActive(false);
+        Debug.Log("Item Picked Up");
+    }
+}
