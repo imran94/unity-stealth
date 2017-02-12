ac4e0ab4353cc903443cfbdeab4e5922253c6a68
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-02-12 23:27:06 +0800

    Level 2 light tweaks

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,12 +7,15 @@ public class Score : MonoBehaviour {
     public Text scoreText;
     private int score;
     public GameObject item1;
+    public GameObject item2;
     private bool item1Check;
+    private bool item2Check;
 
     private void Start()
     {
         score = 0;
         item1Check = false;
+        item2Check = false;
         DisplayScore();
     }
 
@@ -24,6 +27,13 @@ public class Score : MonoBehaviour {
             score = score + 5;
             DisplayScore();
             item1Check = true;
+        }
+
+        if (item2 == null && item2Check == false)
+        {
+            score = score + 5;
+            DisplayScore();
+            item2Check = true;
         }
     }
 
